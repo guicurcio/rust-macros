5b4b6b1188d770b4399f4003cefcd588fb8447e1
g <guidomaurocurcio@gmail.com>
2021-04-05 09:19:29 -0300

    converting types

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+// This is valid syntax going in; a valid rust program going out.
 macro_rules! dec {
-    ($arg1:ty => $arg2:expr; $arg3:path) => {};
+    ($arg1:ty => $arg2:ident) => {
+        type $arg2 = $arg1;
+    };
 }
 
 dec!(
-    u32 => x.foo(); std::path
+    u32 => alsou32
 );
