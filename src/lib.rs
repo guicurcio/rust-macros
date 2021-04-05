d088bdf430f2ba2e1a62989390afab2f5d0c9cc1
g <guidomaurocurcio@gmail.com>
2021-04-05 09:37:23 -0300

    pass block to macro call

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 // This is valid syntax going in; a valid rust program going out.
-macro_rules! dec {
-    ($x:ident) => {
-        $x += 1;
+#[macro_export]
+macro_rules! avec {
+    () => {
+        Vec::new()
     };
 }
 
 #[test]
-fn foo() {
-    let mut x = 42;
-    dec!(x);
-    assert_eq!(x, 43);
+fn empty_vec() {
+    let mut x: Vec<u32> = avec![];
+    assert!(x.is_empty());
 }
