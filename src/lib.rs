24bb7048e3a05081b465501f120243d921a61049
g <guidomaurocurcio@gmail.com>
2021-04-05 09:56:41 -0300

    expand based on repetition

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,31 @@ macro_rules! avec {
     () => {
         Vec::new()
     };
-    ($element:expr) => {{
+    ($($element:expr),+) => {{
         let mut vs = Vec::new();
-        vs.push($element);
+        $(vs.push($element);)*
         vs
     }};
 }
 
 #[test]
 fn empty_vec() {
-    let mut x: Vec<u32> = avec![];
+    let x: Vec<u32> = avec![];
     assert!(x.is_empty());
 }
 #[test]
 fn single() {
-    let mut x: Vec<u32> = avec![42];
+    let x: Vec<u32> = avec![42];
     assert!(!x.is_empty());
     assert_eq!(x.len(), 1);
     assert_eq!(x[0], 42);
+}
+
+#[test]
+fn double() {
+    let x: Vec<u32> = avec![42, 43];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 2);
+    assert_eq!(x[0], 42);
+    assert_eq!(x[1], 43);
 }
