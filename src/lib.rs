697783b41d985ad876bbfa3c391d0a960a905d09
g <guidomaurocurcio@gmail.com>
2021-04-05 09:42:45 -0300

    macros returning blocks

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,22 @@ macro_rules! avec {
     () => {
         Vec::new()
     };
+    ($element:expr) => {{
+        let mut vs = Vec::new();
+        vs.push($element);
+        vs
+    }};
 }
 
 #[test]
 fn empty_vec() {
     let mut x: Vec<u32> = avec![];
     assert!(x.is_empty());
+}
+#[test]
+fn single() {
+    let mut x: Vec<u32> = avec![42];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 1);
+    assert_eq!(x[0], 42);
 }
